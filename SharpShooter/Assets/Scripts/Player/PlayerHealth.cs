677a24cabc7454400f19a826e5fd7845cbe29d98
Max Valle <55932182+Valle94@users.noreply.github.com>
2025-11-23 18:47:22 -0500

    6.34 Player Shield UI
    Created a player health "shield" UI that updates when the player takes damage.

--- SharpShooter/Assets/Scripts/Player/PlayerHealth.cs
+++ SharpShooter/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,11 +1,14 @@
 using Cinemachine;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-    [SerializeField] int startHealth = 5;
+    [Range(1, 10)]
+    [SerializeField] int startHealth = 10;
     [SerializeField] CinemachineVirtualCamera deathVirtualCamera;
     [SerializeField] Transform weaponCamera;
+    [SerializeField] Image[] shieldBars;
 
     int currentHealth;
     int gameOverVirtualCameraPriority = 20;
@@ -13,18 +16,37 @@ public class PlayerHealth : MonoBehaviour
     void Awake()
     {
         currentHealth = startHealth;
+        AdjustShieldUI();
     }
 
     public void TakeDamage(int amount)
     {
         currentHealth -= amount;
-        Debug.Log($"Current Health: {currentHealth}");
+        Debug.Log(currentHealth);
+        
+        AdjustShieldUI();
 
         if (currentHealth <= 0)
         {
             weaponCamera.parent = null;
             deathVirtualCamera.Priority = gameOverVirtualCameraPriority;
             Destroy(this.gameObject);
+        }
+    }
+
+    void AdjustShieldUI()
+    {
+        Debug.Log("Shield Adjusted");
+        for (int i = 0; i < shieldBars.Length; i++)
+        {
+            if (i < currentHealth)
+            {
+                shieldBars[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                shieldBars[i].gameObject.SetActive(false);
+            }
         }
     }
 }
